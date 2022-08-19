d1ebb213571e2f895366bdca4dd2e88ed41dc61a
h8m0ss <danarmenta32@gmail.com>
2022-08-19 00:02:46 -0500

    added constants file

---
+++ src/constants.cpp
@@ -0,0 +1,5 @@
+#include "headings/constants.hpp"
+
+const int Constants::SCREEN_WIDTH = 800;
+const int Constants::SCREEN_HEIGHT = 600;
+const char *Constants::WINDOW_TITLE = "GAME";
---
+++ src/headings/constants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+class Constants
+{
+public:
+    static const int SCREEN_WIDTH;
+    static const int SCREEN_HEIGHT;
+    static const char *WINDOW_TITLE;
+};
