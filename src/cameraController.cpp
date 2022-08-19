31c6da5657f6a03d6d3f0c69e477ac0cb7cef569
h8m0ss <danarmenta32@gmail.com>
2022-08-19 00:04:07 -0500

    added bounds to camera controller

--- src/cameraController.cpp
+++ src/cameraController.cpp
@@ -2,8 +2,13 @@
 
 #include "headings/entityManager.hpp"
 #include "headings/vectorOperations.hpp"
+#include "headings/constants.hpp"
 
-CameraController::CameraController(std::string id) : Entity(id) {}
+CameraController::CameraController(std::string id, Rectangle _cameraBounds)
+    : Entity(id), cameraBounds{_cameraBounds}
+{
+}
+
 CameraController::~CameraController()
 {
     player = nullptr;
@@ -18,10 +23,42 @@ void CameraController::update()
 {
     if (player != nullptr)
     {
-        Vector2 cameraPosition = EntityManager::getInstance()->camera.target;
+        Vector2 cameraPosition{EntityManager::getInstance()->camera.target};
         cameraPosition = VectorOP::lerp(cameraPosition, player->getPosition(), 0.1f);
+
+        Vector2 cameraSize{(float)Constants::SCREEN_WIDTH, (float)Constants::SCREEN_HEIGHT};
+
+        cameraPosition = bindCameraLocation(cameraPosition, cameraSize);
+
         EntityManager::getInstance()->camera.target = cameraPosition;
     }
 }
 
-void CameraController::draw() const {}
+Vector2 CameraController::bindCameraLocation(Vector2 cameraLocation, Vector2 cameraSize)
+{
+    if (cameraBounds.height > 0 || cameraBounds.width > 0)
+    {
+        if (cameraLocation.x - cameraSize.x / 2 < cameraBounds.x)
+        {
+            cameraLocation.x = cameraBounds.x + cameraSize.x / 2;
+        }
+        else if (cameraLocation.x + cameraSize.x / 2 > cameraBounds.x + cameraBounds.width)
+        {
+            cameraLocation.x = cameraBounds.x + cameraBounds.width - cameraSize.x / 2;
+        }
+        if (cameraLocation.y - cameraSize.y / 2 < cameraBounds.y)
+        {
+            cameraLocation.y = cameraBounds.y + cameraSize.y / 2;
+        }
+        else if (cameraLocation.y + cameraSize.y / 2 > cameraBounds.y + cameraBounds.height)
+        {
+            cameraLocation.y = cameraBounds.y + cameraBounds.height - cameraSize.y / 2;
+        }
+    }
+
+    return cameraLocation;
+}
+
+void CameraController::draw() const
+{
+}
--- src/headings/cameraController.hpp
+++ src/headings/cameraController.hpp
@@ -6,9 +6,12 @@ class CameraController : public Entity
 {
 private:
     Player *player;
+    Rectangle cameraBounds;
+
+    Vector2 bindCameraLocation(Vector2 cameraLocation, Vector2 cameraSize);
 
 public:
-    CameraController(std::string id);
+    CameraController(std::string id, Rectangle cameraBounds);
     ~CameraController();
 
     void setup() override;
